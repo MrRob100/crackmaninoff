c708c9c3af8210ad1c6401da13d84e92521b60e2
horidhenry <robarnold100@hotmail.com>
2020-10-12 20:31:59 +0100

    url service added

---
+++ app/Services/UrlService.php
@@ -0,0 +1,23 @@
+<?php
+
+// declare(strict_types=1);
+
+namespace App\Services;
+
+class UrlService
+{
+    public function checkUrl($sub) {
+
+        $sub = $sub.'t';
+
+        if (
+            is_string($sub)
+            && strlen($sub) < 15
+            && ctype_alnum($sub)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
