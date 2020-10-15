1b0cd8aadc1416a062695779eef327af5fdf7af6
horidhenry <robarnold100@hotmail.com>
2020-10-15 18:28:41 +0100

    removed bollocks

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,7 +18,9 @@ class DashboardController extends Controller
      */
 
     public function index($para = "") {
-        
+
+        shell_exec('pwd');
+
         $checker = App::make('App\Services\UrlService');
         $checked = $checker->checkUrl($para);
 
@@ -28,11 +30,11 @@ class DashboardController extends Controller
 
         ini_set('max_post_size', 0);
 
-//        if (!file_exists('storage/data/'.$para)) {
-//            //make it self destruct
-//            mkdir('storage/data/'.$para);
-//            chmod('storage/data/'.$para, 0777);
-//        }
+        if (!file_exists('storage/data/'.$para)) {
+            //make it self destruct
+            mkdir('storage/data/'.$para);
+            chmod('storage/data/'.$para, 0777);
+        }
 
         $para_a = $para == "" ? "" : $para."/";
 
