d39be0cb46b96939463faeec513cebb8211cf6a8
horidhenry <robarnold100@hotmail.com>
2020-12-28 16:35:44 +0000

    permission button

--- resources/views/running.blade.php
+++ resources/views/running.blade.php
@@ -3,43 +3,44 @@
 @section('content')
 
     <h1>RUNNING</h1>
+    <button onclick="request()">Permission</button>
 
 @endsection
 
 <script>
+    
+    function request() {
+        if (typeof DeviceMotionEvent.requestPermission === 'function') {
+            DeviceMotionEvent.requestPermission()
+                .then(permissionState => {
+                    if (permissionState === 'granted') {
 
-    console.log('run');
+                        var lastTimestamp;
+                        var speedX = 0, speedY = 0, speedZ = 0;
+                        window.addEventListener('devicemotion', function(event) {
+                            var currentTime = new Date().getTime();
+                            if (lastTimestamp === undefined) {
+                                lastTimestamp = new Date().getTime();
+                                return; //ignore first call, we need a reference time
+                            }
+                            //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
+                            speedX += event.acceleration.x / 1000 * ((currentTime - lastTimestamp)/1000)/3600;
+                            //... same for Y and Z
+                            lastTimestamp = currentTime;
 
+                            console.log('sx', speedX);
 
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-            .then(permissionState => {
-                if (permissionState === 'granted') {
+                        }, false);
 
-                    var lastTimestamp;
-                    var speedX = 0, speedY = 0, speedZ = 0;
-                    window.addEventListener('devicemotion', function(event) {
-                        var currentTime = new Date().getTime();
-                        if (lastTimestamp === undefined) {
-                            lastTimestamp = new Date().getTime();
-                            return; //ignore first call, we need a reference time
-                        }
-                        //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
-                        speedX += event.acceleration.x / 1000 * ((currentTime - lastTimestamp)/1000)/3600;
-                        //... same for Y and Z
-                        lastTimestamp = currentTime;
-
-                        console.log('sx', speedX);
-
-                    }, false);
-
-
-                }
-            })
-            .catch(console.error);
-    } else {
-        console.log('the else');
+                    }
+                })
+                .catch(console.error);
+        } else {
+            console.log('the else');
+        }
     }
+
+
 
 
 </script>
