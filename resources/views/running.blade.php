b133d9ee873cd302428ce4a8372ac9e47e4efee3
horidhenry <robarnold100@hotmail.com>
2020-12-28 16:25:16 +0000

    permission

--- resources/views/running.blade.php
+++ resources/views/running.blade.php
@@ -10,20 +10,36 @@
 
     console.log('run');
 
-    var lastTimestamp;
-    var speedX = 0, speedY = 0, speedZ = 0;
-    window.addEventListener('devicemotion', function(event) {
-        var currentTime = new Date().getTime();
-        if (lastTimestamp === undefined) {
-            lastTimestamp = new Date().getTime();
-            return; //ignore first call, we need a reference time
-        }
-        //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
-        speedX += event.acceleration.x / 1000 * ((currentTime - lastTimestamp)/1000)/3600;
-        //... same for Y and Z
-        lastTimestamp = currentTime;
 
-        console.log('sx', speedX);
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
 
-    }, false);
+                    var lastTimestamp;
+                    var speedX = 0, speedY = 0, speedZ = 0;
+                    window.addEventListener('devicemotion', function(event) {
+                        var currentTime = new Date().getTime();
+                        if (lastTimestamp === undefined) {
+                            lastTimestamp = new Date().getTime();
+                            return; //ignore first call, we need a reference time
+                        }
+                        //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
+                        speedX += event.acceleration.x / 1000 * ((currentTime - lastTimestamp)/1000)/3600;
+                        //... same for Y and Z
+                        lastTimestamp = currentTime;
+
+                        console.log('sx', speedX);
+
+                    }, false);
+
+
+                }
+            })
+            .catch(console.error);
+    } else {
+        console.log('the else');
+    }
+
+
 </script>
